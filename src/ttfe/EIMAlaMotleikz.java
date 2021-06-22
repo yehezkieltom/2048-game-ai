defd81dd0252d6c41841a1ab913fa63310842c7c
Yehezkiel Priyo Hutomo <yehu00001@stud.uni-saarland.de>
2021-06-22 11:13:35 +0200

    changed some AI parameter

--- src/ttfe/EIMAlaMotleikz.java
+++ src/ttfe/EIMAlaMotleikz.java
@@ -6,12 +6,12 @@ public class EIMAlaMotleikz implements PlayerInterface{
     private double[][] WeightMatrixS;
     private int boardHeight;
     private int boardWidth;
-    private double WeightMatrixBase = 2;
+    private double WeightMatrixBase = 1.7; // make sure are you using pow or multiplication
     private double stuckValue = -100000;
-    private int depth = 6;// best at 3
+    private int depth = 6;// 3 for speed, 6 for precision
     private int moveEfficiencyOffset = 3;
-    private double WeightNumPieceDeltaMultiplier = 10;
-    private double combinationScoreMultiplier = 80;
+    private double NumPieceDeltaMultiplier = 1000;
+    private double combinationScoreMultiplier = 150;// this grows with biggest piece, needs to be scaled up
     private AIWorkspace basisBoardReference;
 
     public MoveDirection getPlayerMove(SimulatorInterface game, UserInterface ui) {
@@ -25,12 +25,14 @@ public class EIMAlaMotleikz implements PlayerInterface{
             if (y_pos % 2 == 0) {
                 for (int x_pos = 0; x_pos < boardWidth; x_pos++) {
                     WeightMatrixS[y_pos][x_pos] = Math.pow(WeightMatrixBase, n);
+                    // WeightMatrixS[y_pos][x_pos] = WeightMatrixBase * n;
                     n++;
                 }
             }
             if (y_pos % 2 == 1) {
                 for (int x_pos = boardWidth - 1; x_pos >= 0; x_pos--) {
                     WeightMatrixS[y_pos][x_pos] = Math.pow(WeightMatrixBase, n);
+                    // WeightMatrixS[y_pos][x_pos] = WeightMatrixBase * n;
                     n++;
                 }
             }
@@ -44,7 +46,7 @@ public class EIMAlaMotleikz implements PlayerInterface{
     private MoveDirection nextMove() {
         int n = depth;
         double bestScore = -1;
-        MoveDirection bestMove =  randomMove();
+        MoveDirection bestMove = randomMove();
         double score = 0;
 
         for (MoveDirection dir: MoveDirection.values()){
@@ -72,8 +74,12 @@ public class EIMAlaMotleikz implements PlayerInterface{
             if (!board.isMovePossible()) {
                 return stuckValue;
             }
+            double patternHeuristic = patternHeuristic(board);
+            double moveEfficiency = moveEfficiency(board);
+            // System.out.println("patternHeuristic: " + patternHeuristic);
+            // System.out.println("moveEfficiency: " + moveEfficiency);
             // System.out.println("Actual Calculation returned");
-            return patternHeuristic(board) + moveEfficiency(board);
+            return patternHeuristic + moveEfficiency;
         }
 
         if (maxPlayer) {
@@ -149,7 +155,7 @@ public class EIMAlaMotleikz implements PlayerInterface{
     }
 
     private double moveEfficiency(AIWorkspace board) {
-        return (basisBoardReference.getNumPieces() - board.getNumPieces() + moveEfficiencyOffset) * WeightNumPieceDeltaMultiplier;
+        return (basisBoardReference.getNumPieces() - board.getNumPieces() + moveEfficiencyOffset) * NumPieceDeltaMultiplier;
     }
 
     private void printGameState(AIWorkspace board) {
